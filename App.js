5befb55bd1e807727942b9afa4fe6f39b308ca90
Gabriel Guivisdalsky <114123989+vichup@users.noreply.github.com>
2023-09-06 10:15:36 -0300

    dale vicenn ponele

---
+++ App.js
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList, Modal, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { AsyncStorage } from 'react-native';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    loadTasksFromStorage();
+  }, []);
+
+  const loadTasksFromStorage = async () => {
+    try {
+      const storedTasks = await AsyncStorage.getItem('tasks');
+      if (storedTasks) {
+        setTasks(JSON.parse(storedTasks));
+      }
+    } catch (error) {
+      console.error('Error loading tasks:', error);
+    }
+  };
+
+  const saveTasksToStorage = async () => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+    } catch (error) {
+      console.error('Error saving tasks:', error);
+    }
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      const newTaskObj = { name: newTask, description: '' };
+      setTasks([...tasks, newTaskObj]);
+      setNewTask('');
+      saveTasksToStorage();
+      setModalVisible(false);
+    }
+  };
+
+  const markTaskComplete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+    saveTasksToStorage();
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+    saveTasksToStorage();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Tareas</Text>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={styles.taskItem}
+            onPress={() => markTaskComplete(index)}
+          >
+            <Text style={item.completed ? styles.completedTask : null}>{item.name}</Text>
+            <TouchableOpacity onPress={() => deleteTask(index)} style={styles.deleteButton}>
+              <Text style={styles.deleteButtonText}>Eliminar</Text>
+            </TouchableOpacity>
+          </TouchableOpacity>
+        )}
+      />
+      <Button title="Agregar Tarea" onPress={() => setModalVisible(true)} />
+
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre de la Tarea"
+            value={newTask}
+            onChangeText={(text) => setNewTask(text)}
+          />
+          <Button title="Agregar" onPress={addTask} />
+          <Button title="Cancelar" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  taskItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  completedTask: {
+    textDecorationLine: 'line-through',
+    color: '#999',
+  },
+  deleteButton: {
+    backgroundColor: 'red',
+    padding: 5,
+    borderRadius: 5,
+  },
+  deleteButtonText: {
+    color: 'white',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 10,
+    fontSize: 16,
+  },
+});
+
+export default App;
